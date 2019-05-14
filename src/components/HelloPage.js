5c581f20f729135792c2a466c6ce72c8400ad29c
Michael <arcaneflorist@gmail.com>
2019-05-14 09:26:13 +0100

    hooks + profile img

--- src/components/HelloPage.js
+++ src/components/HelloPage.js
@@ -13,7 +13,7 @@ import {
   Switch,
 } from 'react-router-dom'
 
-import img from '../assets/ME5.png';
+import img from '../assets/ME7.png';
 import _img from '../assets/smug.png';
 import {useStateValue} from '../AppState';
 
@@ -21,27 +21,23 @@ const arcane = false; //dfdsfsd
 
 //https://66.media.tumblr.com/a4df63bf7f9fd8aa2eae34be6a4ce8e2/tumblr_odd74xENUC1tc5gvpo1_250.png
 
-const Hero = () => {  
-  return(
+const Hero = () => (
   <header class="tc pv4 pv5-ns">
     <img src={img} class="br3 ba b--black-10 h4 w4" alt="avatar"></img>
     <h1 class="f5 f4-ns fw6 black-70">Michael Saunders</h1>    
-    <h2 class="f6 black-70 fw2 ttu tracked">Northampton, UK<p style={{color: '#ff80cc'}}>{'{ .NET, React, Vue, GraphQL, AWS }'}</p></h2>
+    <h2 class="f6 black-70 fw2 ttu tracked">Northampton, UK<p style={{color: '#ff80cc'}}>{'{ .NET, React, GraphQL, AWS }'}</p></h2>
   </header>
-  )
-};
+);
 
-const _Hero = () => {  
-  return(
+const _Hero = () => (  
   <header class="tc pv4 pv5-ns">
     <img src={_img} class="br3 ba b--black-10 h4 w4" alt="avatar"></img>
     <h1 class="f5 f4-ns fw6 black-70">arcaneflorist</h1>    
     <h2 class="f6 black-70 fw2 ttu tracked">Tokyo III <p style={{color: '#ff80cc'}}>{'{ cobwebs && magic }'}</p></h2>
   </header>
-  );
-};
+);
 
-  export default function HelloPage() {
+export default function HelloPage() {
     const [{ theme }, dispatch] = useStateValue();
     
       return (
